cf781448dbe2329b662e4cb9012d933fd1d04a83
astontanion <astontanion1123@gmail.com>
2022-06-12 16:57:00 +0100

    add: utilities class to work with retrofit

---
+++ app/src/main/java/com/example/books/network/BaseUrl.kt
@@ -0,0 +1,3 @@
+package com.example.books.network
+
+const val BASE_API_URL = "https://www.googleapis.com/books/v1/"
---
+++ app/src/main/java/com/example/books/network/DataMessage.kt
@@ -0,0 +1,34 @@
+package com.example.books.network
+
+import android.os.Bundle
+import androidx.core.os.bundleOf
+
+sealed class Reason
+
+sealed class Operation {
+    object POST : Operation()
+    object GET : Operation()
+    object PUT : Operation()
+    object DEL : Operation()
+}
+
+sealed class GenericFailureReason : Reason() {
+    object UNKNOWN : GenericFailureReason()
+    object CONNECTION: GenericFailureReason()
+}
+
+sealed class DataMessage {
+    object None : DataMessage()
+
+    object Waiting : DataMessage()
+
+    data class Success(
+        val operation: Operation
+    ) : DataMessage()
+
+    data class Failure(
+        val operation: Operation,
+        val reason: Reason = GenericFailureReason.UNKNOWN,
+        val extra: Bundle = bundleOf()
+    ) : DataMessage()
+}
